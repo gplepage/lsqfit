4b690f3ae18a17500fe066541d74b8489eb21ec5
G. Peter Lepage <g.p.lepage@cornell.edu>
2018-07-27 18:07:44 -0400

    setup.py and documentation

--- doc/html/_static/documentation_options.js
+++ doc/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '11.0',
+    VERSION: '11.0.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
-LSQFIT_VERSION = '11.0'
+LSQFIT_VERSION = '11.0.1'
 
 from distutils.core import setup
 from distutils.extension import Extension
@@ -84,16 +84,16 @@ ext_modules = [
 
 # distutils
 requires = (
-    ["cython (>=0.17)","numpy (>=1.7)", "scipy (>=0.16)", "gvar (>=8.5)"]
+    ["cython (>=0.17)","numpy (>=1.7)", "scipy (>=0.16)", "gvar (>=9.0)"]
     if USE_CYTHON else
-    ["numpy (>=1.7)", "scipy (>=0.16)", "gvar (>=8.5)"]
+    ["numpy (>=1.7)", "scipy (>=0.16)", "gvar (>=9.0)"]
     )
 
 # pip
 install_requires = (
-    ['cython>=0.17', 'numpy>=1.7', 'scipy>=0.16', 'gvar>=8.5']
+    ['cython>=0.17', 'numpy>=1.7', 'scipy>=0.16', 'gvar>=9.0']
     if USE_CYTHON else
-    ['numpy>=1.7', 'scipy>=0.16', 'gvar>=8.5']
+    ['numpy>=1.7', 'scipy>=0.16', 'gvar>=9.0']
     )
 
 # pypi
